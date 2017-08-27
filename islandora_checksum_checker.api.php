6a3e2c39bfedf5f496e03062611f33855d349505
Mark Jordan <mjordan@sfu.ca>
2017-08-27 16:40:54 +0000

    Work on ISLANDORA-1327.

---
+++ islandora_checksum_checker.api.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @file
+ * This file documents hook functions for the Islandora Checksum Checker module.
+ */
+
+/**
+ * Notify modules of the outcome of the checksum validation.
+ *
+ * @param string $pid
+ *   The current object's PID.
+ * @param string $dsid
+ *   The current datastream's DSID.
+ * @param array $ds_info
+ *   Output of Tuque's APIM getDatastream() method, with 'validateChecksum'
+ *   set to TRUE. See islandora_checksum_checker_validate_checksum() for
+ *   details on the content of this array.
+ * @param string $outcome
+ *   One of 'valid', 'invalid', or 'disabled'.
+ * @param array $exceptions
+ *   An array of all exceptions caught during the validation. Exceptions
+ *   will already be logged to the watchdog.
+ */
+function hook_islandora_checksum_checker_outcome($pid, $dsid, $ds_info, $outcome, $exceptions) {
+  if ($outcome == 'disabled') {
+    watchdog('my_module', 'You should enable checksums for !pid !dsid.',
+      array(
+        '!pid' => $pid,
+        '!dsid' => $dsid,
+      ),
+    WATCHDOG_WARNING);
+  }
+}
